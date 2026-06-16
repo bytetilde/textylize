72e4331111e3fa2a1facb1fd5356ec9fccd4cec9
bytetilde <bytetilde@gmail.com>
2026-06-16 21:25:22 +0300

    i dont need that anymore get this out of here

--- main.py
+++
@@ -1,6 +0,0 @@
-import flask
-app = flask.Flask(__name__)
-@app.route('/', defaults={'path': 'index.html'})
-@app.route('/<path:path>')
-def root(path): return flask.send_from_directory('static', path)
-if(__name__ == '__main__'): app.run('0.0.0.0', 8080)
